b232743982429c798fa31dcfe7ff743617f455b6
rambod <gh.rambod@gmail.com>
2021-06-23 17:32:09 +0430

    Add dist into doc folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   target: 'static',
+  router: {
+    trailingSlash: true
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - ipfsfileuploader',
