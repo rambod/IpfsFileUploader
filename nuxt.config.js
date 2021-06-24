39019f823c002a987bdf5c71b5b5f318ab9508f9
rambod <gh.rambod@gmail.com>
2021-06-24 21:19:56 +0430

    Refactor

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   target: 'static',
   router: {
-    base: "/IpfsFileUploader/"
+    base: "/IpfsFileUploader/",
+    publicPath:"/IpfsFileUploader/",
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
